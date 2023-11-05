baec65aadb6c446add2e0a72ceb86f6f7c51d837
Hillel1408 <etemax@bk.ru>
2023-11-05 16:23:04 +0300

    fix: поправил баг

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -17,7 +17,7 @@ const Search = () => {
   return (
     <div className="p-4 bg-white grid grid-cols-[1fr_auto] gap-3 rounded-xl -translate-y-1/2 md:grid-cols-[1fr]">
       <input
-        type="text"
+        type="number"
         placeholder="Enter an amount"
         className="py-5 px-4 rounded-xl bg-[#F2F2F2] text-[16px] placeholder:text-[#9198A1] text-[#000]"
         value={searchValue ? String(searchValue) : ""}
